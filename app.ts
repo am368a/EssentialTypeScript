9e5970d6823cfd39decdd8407250b205b1b9b73c
Abdul Mohammed <wajidwam@gmail.com>
2021-10-13 12:35:47 -0500

    ES6 features complete

--- app.ts
+++ app.ts
@@ -7,3 +7,28 @@ var array = [
 for (var value of array) {
     console.log(value);
 }
+
+
+// ch 02_07
+
+const itemList = [123, 'pickup dry cleaning', false];
+const [id, title] = itemList;
+
+var a =1, b=5;
+
+[a, b] = [b, a];
+
+const itemListObj = {
+    title: 'xyx'
+};
+
+const {title: tt} = itemListObj;
+
+// ch 08
+
+const calc = (action, ...values) => {
+    switch (action){
+        case 'ADD':
+            return values.reduce((acc, curr)=> acc+curr, 0)
+    }
+};
